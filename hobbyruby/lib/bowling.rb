780c078a225e10d12516e1cdc0717bc7d9dcf5ef
oblove <oblove@02bf09e5-e237-442e-958b-493b9a821bd8>
2008-07-01 06:23:35 +0000

    ボーリング途中
    git-svn-id: https://repos.kakutani.com/svn/oblove@9 02bf09e5-e237-442e-958b-493b9a821bd8

--- hobbyruby/lib/bowling.rb
+++ hobbyruby/lib/bowling.rb
@@ -1,30 +1,41 @@
 class Bowling
   def score(pins)
-
-    frames = []
-    pins.each do |pin|
-      if frames.size == 0 or frames[-1].size == 2
-        frames.push([])
-      end
-
-      frames[-1].push(pin)
-      if pin == 10
-        frames[-1].push(0)
-      end
-    end
+    unpacked = unpack(pins)
 
     total = 0
-    strike_flg = 0
-    frames.each do |frame|
-      frame_score = 0
-      frame.each do |pin| # => 10
-        frame_score += pin
+    is_strike = false
+    each_frame(unpacked) do |f, pins|
+      total += pins.inject(0){|r, i| r + i }
+
+      if pins[0] == 10
+        is_strike = true
       end
-      if frame_score == 10
-        
-      end
-      total += frame_score
     end
+
+#     frames = []
+#     pins.each do |pin|
+#       if frames.size == 0 or frames[-1].size == 2
+#         frames.push([])
+#       end
+
+#       frames[-1].push(pin)
+#       if pin == 10
+#         frames[-1].push(0)
+#       end
+#     end
+
+#     total = 0
+#     strike_flg = 0
+#     frames.each do |frame|
+#       frame_score = 0
+#       frame.each do |pin| # => 10
+#         frame_score += pin
+#       end
+#       if frame_score == 10
+        
+#       end
+#       total += frame_score
+#     end
     
 #     pins.each {|pin|
 #       if strike_flg > 0
@@ -39,5 +50,38 @@ class Bowling
 #       total += pin
 #     }
     return total
+  end
+
+  def unpack(pins)
+    unpacked = []
+    pins.each do |pin|
+      unpacked << pin
+      if pin == 10 && (unpacked.size % 2) == 1 && unpacked.size < 18
+        unpacked << 0
+      end
+    end
+    unpacked << 0 if unpacked.size == 20
+    unpacked
+  end
+
+  def each_frame(unpaked_pins)
+    10.times do |i|
+      case i
+      when 0..8
+        yield i, unpaked_pins[i*2, 2]
+      when 9
+        yield i, unpaked_pins[i*2, 3]
+      end
+    end
+  end
+
+  def is_strike?(pins, index)
+    is_first_throw = true
+    pins.each_with_index do |pin, i|
+      if is_first_throw && pin == 10
+      else
+        is_first_throw = false
+      end
+    end
   end
 end
--- hobbyruby/test/bowling_test.rb
+++ hobbyruby/test/bowling_test.rb
@@ -18,16 +18,95 @@ class BowlingTest < Test::Unit::TestCase
     assert_equal(20 , game.score(pins))
   end
 
-  def test_strike
+#   def test_strike
+#     game = Bowling.new
+#     pins = []
+#     19.times {|frame| pins[frame] = 0}
+#     pins[0] = 10
+#     pins[1] = 5
+#     pins[2] = 3
+#     assert_equal(26, game.score(pins))
+#   end
+
+  def test_unpack
     game = Bowling.new
     pins = []
-    18.times {|frame| pins[frame] = 0}
+    19.times {|frame| pins[frame] = 0}
     pins[0] = 10
     pins[1] = 5
     pins[2] = 3
-    assert_equal(26, game.score(pins))
+    assert_equal([10, 0, 5, 3, 0, 0, 0, 0, 0, 0,
+                   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], game.unpack(pins))
   end
 
+  def test_unpack_gatter_and_spare
+    game = Bowling.new
+    pins = []
+    20.times {|frame| pins[frame] = 0}
+    pins[0] = 0
+    pins[1] = 10    
+    pins[2] = 5
+    pins[3] = 3
+    assert_equal([ 0, 10, 5, 3, 0, 0, 0, 0, 0, 0,
+                   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], game.unpack(pins))
+  end
+
+  def test_unpack_when_three_throw_in_final_frame
+    game = Bowling.new
+    pins = []
+    21.times {|frame| pins[frame] = 0}
+    pins[18] = 10
+    pins[19] = 5
+    pins[20] = 3
+    assert_equal([0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                  0, 0, 0, 0, 0, 0, 0, 0, 10, 5, 3], game.unpack(pins))
+  end
+
+  def test_unpack_when_three_throw_in_final_frame_2
+    game = Bowling.new
+    pins = []
+    21.times {|frame| pins[frame] = 0}
+    pins[18] = 5
+    pins[19] = 5
+    pins[20] = 10
+    assert_equal([0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                  0, 0, 0, 0, 0, 0, 0, 0, 5, 5, 10], game.unpack(pins))
+  end
+
+  def test_unpack_when_three_throw_in_final_frame_3
+    game = Bowling.new
+    pins = []
+    21.times {|frame| pins[frame] = 0}
+    pins[18] = 0
+    pins[19] = 10
+    pins[20] = 5
+    assert_equal([0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                  0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 5], game.unpack(pins))
+  end
+
+  def test_each_frame
+    game = Bowling.new
+
+    unpacked = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 5]
+    
+    game.each_frame(unpacked) do |f, pins|
+      case f
+      when 0..8
+        assert_equal([0, 0], pins)
+      when 9
+        assert_equal([0, 10, 5], pins)
+      end
+    end
+  end
+
+  def test_is_strike
+    game = Bowling.new
+    pins = [10, 1, 1, 1, 1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    assert(game.is_strike?(pins, 0))
+  end
+  
 #   def test_spare
 #     game = Bowling.new
 #     pins = []
