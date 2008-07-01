030684d711a9a4a1684bedb582e2c8deeb68530b
oblove <oblove@02bf09e5-e237-442e-958b-493b9a821bd8>
2008-07-01 05:16:44 +0000

    1coma end
    git-svn-id: https://repos.kakutani.com/svn/oblove@7 02bf09e5-e237-442e-958b-493b9a821bd8

---
+++ hobbyruby/lib/nabeatsu.rb
@@ -0,0 +1,44 @@
+class Nabeatsu
+  AHO_DIGIT_1 = {
+    1=>"い〜ち",
+    2=>"に〜",
+    3=>"さ〜ん",
+    4=>"よ〜ん",
+    5=>"ご〜",
+    6=>"ろ〜く",
+    7=>"な〜な",
+    8=>"は〜ち",
+    9=>"きゅ〜"
+  }
+ AHO_DIGIT_2 = {
+    1=>"じゅ〜",
+    2=>"に〜じゅ〜",
+    3=>"さ〜んじゅ〜",
+    4=>"よ〜んじゅ〜",
+    5=>"ご〜じゅ〜",
+    6=>"ろ〜くじゅ〜",
+    7=>"な〜なじゅ〜",
+    8=>"は〜ちじゅ〜",
+    9=>"きゅ〜じゅ〜"
+  }
+
+  def say(number)
+
+    if number % 3 == 0 then
+      say_aho(number)
+    elsif number.to_s[/3/] then
+      say_aho(number)
+    else
+      number.to_s
+    end
+  end
+
+  def say_aho(number)
+    if number < 10
+      AHO_DIGIT_1[number]
+    else
+      AHO_DIGIT_2[number.to_s[0,1].to_i] +
+        AHO_DIGIT_1[number.to_s[1,1].to_i]
+    end
+  end
+end
---
+++ hobbyruby/test/nabeatsu_test.rb
@@ -0,0 +1,49 @@
+$:.unshift(File.expand_path("../lib", File.dirname(__FILE__)))
+require 'test/unit'
+require 'nabeatsu'
+class NabeatsuTest < Test::Unit::TestCase
+
+  def setup
+    @nabe = Nabeatsu.new
+  end
+
+  def test_say
+    assert_equal("1",@nabe.say(1))
+  end
+
+  def test_say2
+    assert_equal("2",@nabe.say(2))
+  end
+
+  def test_say3
+    assert_equal("さ〜ん", @nabe.say(3))
+  end
+
+  def test_say6
+    assert_equal("ろ〜く", @nabe.say(6))
+  end
+
+  def test_say9
+    assert_equal("きゅ〜", @nabe.say(9))
+  end
+
+  def test_say12
+    assert_equal("じゅ〜に〜", @nabe.say(12))
+  end
+
+  def test_say13
+    assert_equal("じゅ〜さ〜ん", @nabe.say(13))
+  end
+
+  def test_say_aho1
+    assert_equal("い〜ち", @nabe.say_aho(1))
+  end
+
+  def test_say_aho2
+    assert_equal("に〜", @nabe.say_aho(2))
+  end
+
+  def test_say_aho12
+    assert_equal("じゅ〜に〜", @nabe.say_aho(12))
+  end
+end
