0ccff9755c825035fa7d0f8f12447f9fd04b4a6b
oblove <oblove@02bf09e5-e237-442e-958b-493b9a821bd8>
2008-07-01 07:10:09 +0000

    で，リファクタリングしたよ．
    git-svn-id: https://repos.kakutani.com/svn/oblove@14 02bf09e5-e237-442e-958b-493b9a821bd8

--- hobbyruby/lib/bowling.rb
+++ hobbyruby/lib/bowling.rb
@@ -1,27 +1,32 @@
 class Bowling
+  FRAME_SIZE = 10
+    
   def score(pins)
+    @pins = pins
 
     total = 0
     index = 0
-    10.times do |frame|
-      # ストライク
-      if pins[index] == 10
-        total += pins[index] + pins[index + 1] + pins[index + 2]
+    FRAME_SIZE.times do |frame|
+      if strike?(index)
+        total += 10 + @pins[index + 1] + @pins[index + 2]
         index += 1
-
-        # スペア
-      elsif pins[index] + pins[index + 1] == 10
-        total += pins[index] + pins[index + 1] + pins[index + 2]
+      elsif spare?(index)
+        total += 10 + @pins[index + 2]
         index += 2
-
-      # 普通の場合
-
       else
-        total += pins[index] + pins[index + 1]
+        total += @pins[index] + @pins[index + 1]
         index += 2        
       end
     end
     
     return total
+  end
+
+  def strike?(index)
+    @pins[index] == 10
+  end
+
+  def spare?(index)
+    @pins[index] + @pins[index + 1] == 10
   end
 end
